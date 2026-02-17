08242c6c5b898487be85d9b46e5a8e6b446a2e30
Raymond Gigliotti <ray@firestitch.com>
2026-02-17 09:22:24 -0500

    Update package-lock.json and package.json for version bump; add style version export and update SCSS variable for dialog text color.Update package-lock.json and package.json for version bump; add style version export and update SCSS variable for dialog text color.

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,5 @@
-
+/**
+ * @firestitch/style - Shared styles and variables.
+ * Import SCSS from '@firestitch/style/styles' or include in angular.json styles.
+ */
+export const STYLE_VERSION = '18.0.54';
